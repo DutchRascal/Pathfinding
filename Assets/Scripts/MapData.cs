cd8c806d7444343fc373183f315404d66cd47abd
DutchRascal <DutchRascal@fritz.box>
2020-01-24 17:59:08 +0100

    Pathfinding-014

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -11,9 +11,23 @@ public class MapData : MonoBehaviour
         width = 10,
         height = 5;
     public TextAsset textAsset;
+    public Texture2D textureMap;
     public string resourcePath = "Mapdata";
 
-    public List<string> GetTextFromFile(TextAsset tAsset)
+    private void Start()
+    {
+        string levelName = SceneManager.GetActiveScene().name;
+        if (textureMap == null)
+        {
+            textureMap = Resources.Load(resourcePath + "/" + levelName) as Texture2D;
+        }
+        if (textAsset == null)
+        {
+            textAsset = Resources.Load(resourcePath + "/" + levelName) as TextAsset;
+        }
+    }
+
+    public List<string> GetMapFromTextFile(TextAsset tAsset)
     {
         List<string> lines = new List<string>();
         if (tAsset != null)
@@ -23,21 +37,47 @@ public class MapData : MonoBehaviour
             lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
             lines.Reverse();
         }
-        else
-        {
-            Debug.LogWarning("MAPDATA GetTextFromFile Error: invalid TextAsset");
-        }
+
         return lines;
     }
 
-    public List<string> GetTextFromFile()
+    public List<string> GetMapFromTextFile()
     {
         if (textAsset == null)
         {
             string levelName = SceneManager.GetActiveScene().name;
             textAsset = Resources.Load(resourcePath + "/" + levelName) as TextAsset;
         }
-        return GetTextFromFile(textAsset);
+        return GetMapFromTextFile(textAsset);
+    }
+
+    public List<string> GetMapFromTexture(Texture2D texture)
+    {
+        List<string> lines = new List<string>();
+        if (texture != null)
+        {
+            for (int y = 0; y < texture.height; y++)
+            {
+                string newLine = "";
+                for (int x = 0; x < texture.width; x++)
+                {
+                    if (texture.GetPixel(x, y) == Color.black)
+                    {
+                        newLine += '1';
+                    }
+                    else if (texture.GetPixel(x, y) == Color.white)
+                    {
+                        newLine += '0';
+                    }
+                    else
+                    {
+                        newLine += ' ';
+                    }
+                }
+                lines.Add(newLine);
+            }
+        }
+        return lines;
     }
 
     public void SetDimensions(List<string> textLines)
@@ -55,7 +95,14 @@ public class MapData : MonoBehaviour
     public int[,] MakeMap()
     {
         List<string> lines = new List<string>();
-        lines = GetTextFromFile();
+        if (textureMap != null)
+        {
+            lines = GetMapFromTexture(textureMap);
+        }
+        else
+        {
+            lines = GetMapFromTextFile();
+        }
         SetDimensions(lines);
         int[,] map = new int[width, height];
         for (int y = 0; y < height; y++)
