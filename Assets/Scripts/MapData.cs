ca9ea5871c54bec7a6a635c26b47c067e484bc8f
DutchRascal <DutchRascal@fritz.box>
2020-01-24 13:28:01 +0100

    Pathfinding-011

--- Assets/Scripts/MapData.cs
+++ Assets/Scripts/MapData.cs
@@ -8,6 +8,29 @@ public class MapData : MonoBehaviour
     public int
         width = 10,
         height = 5;
+    public TextAsset textAsset;
+
+    public List<string> GetTextFromFile(TextAsset tAsset)
+    {
+        List<string> lines = new List<string>();
+        if (tAsset != null)
+        {
+            string textData = tAsset.text;
+            string[] delimiters = { "\r\n", "\n" };
+            lines.AddRange(textData.Split(delimiters, System.StringSplitOptions.None));
+            lines.Reverse();
+        }
+        else
+        {
+            Debug.LogWarning("MAPDATA GetTextFromFile Error: invalid TextAsset");
+        }
+        return lines;
+    }
+
+    public List<string> GetTextFromFile()
+    {
+        return GetTextFromFile(textAsset);
+    }
 
     public int[,] MakeMap()
     {
@@ -19,23 +42,6 @@ public class MapData : MonoBehaviour
                 map[x, y] = 0;
             }
         }
-        map[1, 0] = 1;
-        map[1, 1] = 1;
-        map[1, 2] = 1;
-        map[3, 2] = 1;
-        map[3, 3] = 1;
-        map[3, 4] = 1;
-        map[4, 2] = 1;
-        map[5, 1] = 1;
-        map[5, 2] = 1;
-        map[6, 2] = 1;
-        map[6, 3] = 1;
-        map[8, 0] = 1;
-        map[8, 1] = 1;
-        map[8, 2] = 1;
-        map[8, 4] = 1;
-
-
         return map;
     }
 
