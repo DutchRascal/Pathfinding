83bfe0983df6f7bbc775bd568140d9678275ac2b
DutchRascal <DutchRascal@fritz.box>
2020-01-31 15:21:04 +0100

    Pathfinding-028

--- Assets/Scripts/Pathfinder.cs
+++ Assets/Scripts/Pathfinder.cs
@@ -9,7 +9,7 @@ public class Pathfinder : MonoBehaviour
     Node m_goalNode;
     Graph m_graph;
     GraphView m_graphView;
-    Queue<Node> m_frontierNodes;
+    PriorityQueue<Node> m_frontierNodes;
     List<Node>
         m_exploreNodes,
         m_pathNodes;
@@ -59,7 +59,7 @@ public class Pathfinder : MonoBehaviour
 
         ShowColors(graphView, start, goal);
 
-        m_frontierNodes = new Queue<Node>();
+        m_frontierNodes = new PriorityQueue<Node>();
         m_frontierNodes.Enqueue(start);
         m_exploreNodes = new List<Node>();
         m_pathNodes = new List<Node>();
@@ -202,6 +202,7 @@ public class Pathfinder : MonoBehaviour
                     float newDistanceTraveled = distanceToNeighbor + node.distanceTraveled;
                     node.neighbors[i].distanceTraveled = newDistanceTraveled;
                     node.neighbors[i].previous = node;
+                    node.neighbors[i].priority = m_exploreNodes.Count;
                     m_frontierNodes.Enqueue(node.neighbors[i]);
                 }
             }
@@ -225,6 +226,7 @@ public class Pathfinder : MonoBehaviour
                     }
                     if (!m_frontierNodes.Contains(node.neighbors[i]))
                     {
+                        node.neighbors[i].priority = (int)node.neighbors[i].distanceTraveled;
                         m_frontierNodes.Enqueue(node.neighbors[i]);
                     }
                 }
